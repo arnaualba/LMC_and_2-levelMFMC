lasso fixed N with normalisation method and correct rmse calculation for LMC

src/LMC.py
@@ -161,10 +161,12 @@ class LMC():
         
         std_low_fi = low_fi_samps.std(ddof=1)
         std_hi_fi = hi_fi_samps.std(ddof=1)
-        cov_squared = np.cov(low_fi_samps**2, hi_fi_samps**2)[0,1]
+        cov_squared = np.cov((low_fi_samps - low_fi_samps.mean())**2,
+                             (hi_fi_samps - hi_fi_samps.mean())**2)[0,1]
         
         rmse_MC = std_hi_fi / 2 / np.sqrt(N)
-        rmse_MFMC = rmse_MC * np.sqrt(1 + std_low_fi**4 / std_hi_fi**4 - cov_squared / std_hi_fi**4)
+        intermediate_quantity = 1 + std_low_fi**4 / std_hi_fi**4 - cov_squared / std_hi_fi**4
+        rmse_MFMC = rmse_MC * np.sqrt(intermediate_quantity) if intermediate_quantity > 0.0 else 0.0
         return rmse_MC, rmse_MFMC
         
     def get_estimates(self, Xtrain, ytrain, Xtest, ytest_low_fi = [], ytrain_low_fi = []):
@@ -187,11 +189,6 @@ class LMC():
         self.Xtr_ = Xtrain
         self.ytr_ = ytrain
         N = len(self.ytr_)
-
-        assert self.Xtr_.shape[1] == self.Xte_.shape[1]
-        assert self.Xtr_.shape[0] == self.ytr_.shape[0]
-        
-        self.__print_num_samples__()
         
         starttime = time.time()
         
@@ -234,6 +231,11 @@ class LMC():
                 print('with estimated LMC errors: {:.3e}, {:.3e}'.format(errorsLMC[0], errorsLMC[1]))
 
             return results
+
+        
+        assert self.Xtr_.shape[1] == self.Xte_.shape[1]
+        assert self.Xtr_.shape[0] == self.ytr_.shape[0]
+        self.__print_num_samples__()
         
         # Scale data, to make ML methods work better:
         self.ytr_ = (self.ytr_ - meanMC) / np.sqrt(varMC)

src/LassoFixedN.py
@@ -9,14 +9,18 @@ class LassoFixedN():
     Parameters:
     Nfeat : float. If Nfeat > 1, number of nonzero coefficients.
     If Nfeat < 1.0, the number of nonzero coefficients is Nfeat*Ntrain
+    normalise_output : boolean. If True, there is no need to normalise the outputs
+    y, since fit() and predict() will take care of it.
     
     Arguments:
     desiredNfeat_ : desired number of nonzero coefficients.
-    Nfeat_ : real number of nonzero coefficients.
+    normalise_output_ : whether to normalise the outputs y when training and predicting.
+    Nfeat_ : true number of nonzero coefficients.
     alpha_ : Chosen best alpha
     alphas_ : array of alphas used in the lasso_path
-    intercept_ : 
+    intercept_ : mean of training data.
     coef_ : coefficients from the Lasso fit.
+    sig_ : std of training data.
     
     Functions:
     fit(X,y,**params) : Computes the LassoPath with X,y, and chooses the model with the desired number of features.
@@ -24,9 +28,10 @@ class LassoFixedN():
     predict(X)
     '''
     
-    def __init__(self, Nfeat = 0.8):
+    def __init__(self, Nfeat = 0.8, normalise_output = False):
         super().__init__()
         self.desiredNfeat_ = Nfeat
+        self.normalise_output_ = normalise_output
         
         self.Nfeat_ = 0
         self.coef_ = []
@@ -41,7 +46,10 @@ class LassoFixedN():
         
         # Lasso path and find the path with the correct number of features.
         self.intercept_ = y.mean()
-        self.alphas_, coef_paths, _ = lasso_path(X, y - self.intercept_, **params)
+        self.sig_ = y.std(ddof=1) if self.normalise_output_ else 1.0
+        self.alphas_, coef_paths, _ = lasso_path(X,
+                                                 (y - self.intercept_) / self.sig_,
+                                                 **params)
         Nalpha = len(self.alphas_)
         Nfeats = np.array([len(np.where(coef_paths[:,idx] != 0.0)[0]) for idx in range(Nalpha)])
         idx = np.argmin(np.abs(Nfeats - targetN))
@@ -52,13 +60,14 @@ class LassoFixedN():
         self.Nfeat_ = len(np.where(coef_paths[:,idx] != 0.0)[0])
     
     def predict(self, X):
-        return np.dot(X, self.coef_) + self.intercept_
+        return np.dot(X, self.coef_) * self.sig_ + self.intercept_
 
     def get_params(self):
         params = {'numFeat' : self.Nfeat_}   
         params = {'desiredNumFeat' : self.desiredNfeat_}
         params['coef'] = self.coef_
         params['intercept'] = self.intercept_
+        params['sig'] = self.sig_
         params['alphas'] = self.alphas_ 
         params['alpha'] = self.alpha_
         