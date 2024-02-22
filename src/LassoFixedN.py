6f4d0a90dc96940b6ae80a5df89cce1e98a246a7
Arnau Albà <arnau.albajacas@psi.ch>
2024-02-22 07:32:26 +0100

    Custom output scaler for LassoFixedN

--- src/LassoFixedN.py
+++ src/LassoFixedN.py
@@ -4,23 +4,23 @@ from sklearn.linear_model import lasso_path
 class LassoFixedN():
     '''
     Lasso model where alpha is tuned such that the number of nonzero coefs is N.
-    The Lasso path is computed on the training data, then alpha is chosen when
-    
+    The Lasso path is computed on the training data, then alpha is chosen such that supp(coef_) = N,
+    where supp is the support.
+    Note: at the moment there are a bunch of internal reshape() operations. In the future this should be done in a nicer way.
+
     Parameters:
     Nfeat : float. If Nfeat > 1, number of nonzero coefficients.
     If Nfeat < 1.0, the number of nonzero coefficients is Nfeat*Ntrain
-    normalise_output : boolean. If True, there is no need to normalise the outputs
+    output_scaler : None or a scaler from sklearn.preprocessing (e.g. StandardScaler or PowerTransformer, necessary since Lasso assumes a "linear model with Gaussian noise")
     y, since fit() and predict() will take care of it.
     
     Arguments:
     desiredNfeat_ : desired number of nonzero coefficients.
-    normalise_output_ : whether to normalise the outputs y when training and predicting.
+    output_scaler_ : None or a scaler from sklearn.preprocessing (e.g. StandardScaler or PowerTransformer, necessary since Lasso assumes a "linear model with Gaussian noise")
     Nfeat_ : true number of nonzero coefficients.
     alpha_ : Chosen best alpha
     alphas_ : array of alphas used in the lasso_path
-    intercept_ : mean of training data.
     coef_ : coefficients from the Lasso fit.
-    sig_ : std of training data.
     
     Functions:
     fit(X,y,**params) : Computes the LassoPath with X,y, and chooses the model with the desired number of features.
@@ -28,14 +28,13 @@ class LassoFixedN():
     predict(X)
     '''
     
-    def __init__(self, Nfeat = 0.8, normalise_output = False):
+    def __init__(self, Nfeat = 0.8, output_scaler = None):
         super().__init__()
         self.desiredNfeat_ = Nfeat
-        self.normalise_output_ = normalise_output
+        self.output_scaler_ = output_scaler
         
         self.Nfeat_ = 0
         self.coef_ = []
-        self.intercept_ = 0.0
         self.alphas_ = []
         self.alpha_ = 0.0
         
@@ -43,13 +42,18 @@ class LassoFixedN():
         # Find target number of features.
         N = X.shape[0]
         targetN = int(self.desiredNfeat_) if self.desiredNfeat_ > 1.0 else int(self.desiredNfeat_ * N)
-        
+
+        # Preprocessing of output data:
+        if self.output_scaler_ != None:
+            y_transformed = y.reshape(-1,1)
+            self.output_scaler_.fit(y_transformed)
+            y_transformed = self.output_scaler_.transform(y_transformed).reshape(-1)
+        else:
+            y_transformed = y.copy()
+            
         # Lasso path and find the path with the correct number of features.
-        self.intercept_ = y.mean()
-        self.sig_ = y.std(ddof=1) if self.normalise_output_ else 1.0
-        self.alphas_, coef_paths, _ = lasso_path(X,
-                                                 (y - self.intercept_) / self.sig_,
-                                                 **params)
+        self.alphas_, coef_paths, _ = lasso_path(X, y_transformed, **params)
+        
         Nalpha = len(self.alphas_)
         Nfeats = np.array([len(np.where(coef_paths[:,idx] != 0.0)[0]) for idx in range(Nalpha)])
         idx = np.argmin(np.abs(Nfeats - targetN))
@@ -60,25 +64,16 @@ class LassoFixedN():
         self.Nfeat_ = len(np.where(coef_paths[:,idx] != 0.0)[0])
     
     def predict(self, X):
-        return np.dot(X, self.coef_) * self.sig_ + self.intercept_
+        if self.output_scaler_ != None:
+            return self.output_scaler_.inverse_transform(np.dot(X, self.coef_).reshape(-1,1)).reshape(-1)
+        else:
+            return np.dot(X, self.coef_)
 
     def get_params(self, deep = False):
         params = {'numFeat' : self.Nfeat_}   
         params = {'desiredNumFeat' : self.desiredNfeat_}
         params['coef'] = self.coef_
-        params['intercept'] = self.intercept_
-        params['sig'] = self.sig_
         params['alphas'] = self.alphas_ 
         params['alpha'] = self.alpha_
         
         return params
-
-    def save_model(self, fn):
-        arr_to_save = np.append(self.coef_, [self.sig_, self.intercept_])
-        np.save(fn, arr_to_save)
-
-    def load_model(self, fn):
-        arr = np.load(fn)
-        self.coef_ = arr[:-2]
-        self.sig_ = arr[-2]
-        self.intercept_ = arr[-1]
